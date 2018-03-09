2a7a498dac940d79c3eddacd683a988a7c16ca27
amazimgboycot <amazimgboycot@github.com>
2018-03-10 00:24:23 +0800

    修改购物车功能

--- pages/shop-cart/index.js
+++ pages/shop-cart/index.js
@@ -8,41 +8,46 @@ Page({
   data: {
     goodsList:[],
     selectAll:true,
-    totalCount: 0
+    totalPrice: 0,
+    totalCount:0
   },
+  //全选
   changeSelectAll(){
-    let totalCount = 0;
+    let totalPrice = 0;
     this.data.goodsList.map(val => {
       val.isSelect = !this.data.selectAll;
-      totalCount += val.selNum*val.minPrice;
+      totalPrice += val.selNum*val.minPrice;
     })
     this.setData({
       selectAll: !this.data.selectAll,
       goodsList: this.data.goodsList
     })
     this.data.selectAll ? this.setData({
-      totalCount: totalCount
+      totalPrice: totalPrice
     }) : this.setData({
-        totalCount: 0
+        totalPrice: 0
     })
   },
+  //单选
   changeSelect(e) {
     console.log(e.target.dataset)
     let isSelectAll = true;
-    let tempCount = 0;
+    let tempPrice = 0;
     this.data.goodsList.map((val,index) => {
       if (e.target.dataset.index==index) {
         val.isSelect = !val.isSelect;
         if (val.isSelect == false) {
           isSelectAll = false;
-          tempCount = -val.selNum * val.minPrice;
+          tempPrice = -val.selNum * val.minPrice;
         } else {
-          tempCount = val.selNum * val.minPrice;
+          tempPrice = val.selNum * val.minPrice;
         }
-      }
-      
+      }else if (val.isSelect == false){
+          isSelectAll = false;
+        }    
     })
     if (isSelectAll){
+      console.log(isSelectAll)
       this.setData({
         selectAll: true
       })
@@ -53,7 +58,7 @@ Page({
     }
     this.setData({
       goodsList: this.data.goodsList,
-      totalCount: this.data.totalCount + tempCount
+      totalPrice: this.data.totalPrice + tempPrice
     })
   },
   turnToHome(){
@@ -61,43 +66,65 @@ Page({
      url: '/pages/index/index',
    })
   },
-
+  //增加商品数量
   addCount(e) {
     let selnum = e.target.dataset.selnum;
     let color = e.target.dataset.color;
     let name = e.target.dataset.name;
+    let tempPrice = 0;
     this.data.goodsList.map(val=>{
       if (val.selNum == selnum && name == val.name && val.colorOrSize == color){
         val.selNum +=1;
+        tempPrice =  val.minPrice;
       }
     })
+    this.setData({
+      totalPrice: this.data.totalPrice + tempPrice,
+      goodsList: this.data.goodsList,
+      totalCount: ++this.data.totalCount
+    })
     wx.setStorage({
-      key: 'cartData',
+      key: 'cartData', 
       data: this.data.goodsList
     })
-    this.setData({
-      goodsList: this.data.goodsList
+    
+    wx.setStorage({
+      key: 'totalCount',
+      data: this.data.totalCount
     })
+   
   },
+  //减少商品数量
   reduceCount(e) {
     let selnum = e.target.dataset.selnum;
     let color = e.target.dataset.color;
     let name = e.target.dataset.name;
+    let tempPrice = 0;
     this.data.goodsList.map(val => {
       if (val.selNum == selnum && name == val.name && val.colorOrSize == color) {
         val.selNum -= 1;
-        if (val.selNum<=1){
+        tempPrice = val.minPrice;
+        if (val.selNum<1){
           val.selNum = 1;
+          tempPrice = 0;
         }
       }
+    })
+    console.log(this.data.totalPrice-tempPrice)
+    this.setData({
+      totalPrice: this.data.totalPrice - tempPrice,
+      goodsList: this.data.goodsList,
+      totalCount: tempPrice ? --this.data.totalCount : this.data.totalCount
     })
     wx.setStorage({
       key: 'cartData',
       data: this.data.goodsList
     })
-    this.setData({
-      goodsList: this.data.goodsList
+    wx.setStorage({
+      key: 'totalCount',
+      data: this.data.totalCount
     })
+    
   },
 
   getData(){
@@ -114,19 +141,30 @@ Page({
             goodsList: [res.data]
           })
         }
-        this.data.goodsList.map(val=>{
-          this.setData({
-            totalCount:this.data.totalCount+=val.selNum * val.minPrice
-          })
+        let totalPrice = 0;
+        this.data.goodsList.map(val => {
+          totalPrice += val.selNum * val.minPrice;
+        })
+        this.setData({
+          selectAll: true,
+          totalPrice
         })
       },
+    })
+    wx.getStorage({
+      key: 'totalCount',
+      success: res => {
+        this.setData({
+          totalCount:res.data
+        })
+      }
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    this.getData()
+    
   },
 
   /**
@@ -140,21 +178,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    wx.getStorage({
-      key: 'cartData',
-      success: res => {
-        // console.log(res.data)
-        if (res.data instanceof Array) {
-          this.setData({
-            goodsList: res.data
-          })
-        } else {
-          this.setData({
-            goodsList: [res.data]
-          })
-        }
-      },
-    })
+    this.getData()
   },
 
   /**
